eb2eff84cfc1b9893012229f83e973c0baa7f080
dnfd1 <womp>
2025-10-04 22:41:06 -0700

    change login screen add api post (no pr)

--- roommate-app/src/screens/LoginScreen.js
+++ roommate-app/src/screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -7,11 +7,14 @@ import {
   TextInput,
   SafeAreaView,
 } from 'react-native';
-var username;
-var password;
+
 // using navigation stack is fine for now but when home screen is called this navigation stack should be thrown out
 // and replaced with a navbar and a new navigation stack to avoid returning here after a login
 export default function LoginScreen({ navigation }) {
+  const API_URL = 'https://localhost:8481/api/login'
+  // api params
+  const [userNameData, setUserNameData] = useState('');
+  const [passwordData, setPasswordData] = useState('');
   return (
     // move these text inputs into component files later for now we will store data here though
     <SafeAreaView style={styles.container}>
@@ -20,24 +23,41 @@ export default function LoginScreen({ navigation }) {
         <TextInput
           style={styles.textinput} 
           placeholder="Username"
-          value={username}
+          value={userNameData}
+          onChangeText={setUserNameData}
         />
         <TextInput
           style={styles.textinput} 
           secureTextEntry={true}
           placeholder="Password"
-          value={password}
+          value={passwordData}
+          onChangeText={setPasswordData}
         />
         <TouchableOpacity 
           style={styles.button}
           // straight back to the welcome screen until we get a home screen
           onPress={() => 
-            {
+          {
+
+            fetch(API_URL, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                user: userNameData,
+                pass: passwordData,
+              }),
+            })
+              .then(response => response.json()) 
+              .then(data => console.log('Success:', data))
+              .catch(error => console.error('Error:', error));
+            navigation.navigate('Welcome')
             // call api
             // check result for session token; store session token in storage for home screen
             // if api does not respond with one then throw error
-            navigation.navigate('Welcome')
-            }
+            
+          }
         }
         >
           <Text style={styles.buttonText}>Log in</Text>
